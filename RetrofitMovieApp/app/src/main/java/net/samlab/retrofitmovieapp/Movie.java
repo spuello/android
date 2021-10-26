d439f1f422969174a539a8431c1a11a5baa198e2
Samuel Puello <spuellocoronado@gmail.com>
2021-10-26 18:09:37 -0400

    Initial retrofit movie app configuration

---
+++ RetrofitMovieApp/app/src/main/java/net/samlab/retrofitmovieapp/Movie.java
@@ -0,0 +1,19 @@
+package net.samlab.retrofitmovieapp;
+
+public class Movie {
+    private int id;
+    private String name;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+}
---
+++ RetrofitMovieApp/app/src/main/java/net/samlab/retrofitmovieapp/MovieApi.java
@@ -0,0 +1,8 @@
+package net.samlab.retrofitmovieapp;
+
+import retrofit2.http.GET;
+
+public interface MovieApi {
+    @GET("");
+    
+}
